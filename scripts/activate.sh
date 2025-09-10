13c167b72fabc59513f8e7befcae3c9e4c7e29b2
Anish_G <83379728+neur0n-7@users.noreply.github.com>
2025-09-10 10:26:42 -0700

    Change username and email in scripts/activate.sh

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -1,5 +1,5 @@
-export GIT_USER_NAME="jm1021"
-export GIT_USER_EMAIL="jmort1021@gmail.com"
+export GIT_USER_NAME="neur0n-7"
+export GIT_USER_EMAIL="anishgupta7500@gmail.com"
 
 # Function to check if a line exists in run commands
 line_exists_in_rc() {
